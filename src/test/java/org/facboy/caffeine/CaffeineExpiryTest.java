c8420fbd76af54e88126131ef717cc386c58093e
Christopher Ng <facboy@gmail.com>
2020-11-30 17:38:50 +0000

    updated dependency versions

--- src/test/java/org/facboy/caffeine/CaffeineExpiryTest.java
+++ src/test/java/org/facboy/caffeine/CaffeineExpiryTest.java
@@ -2,20 +2,15 @@ package org.facboy.caffeine;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.sameInstance;
-
-import java.time.Duration;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Expiry;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
 
 class CaffeineExpiryTest {
 
     @Test
-    @SuppressWarnings({"unchecked", "RedundantStringConstructorCall", "InfiniteLoopStatement"})
     void maxExpiryShouldNotExpireImmediately() {
         LoadingCache<Integer, String> cache = Caffeine.newBuilder()
                 .expireAfter(new Expiry<Integer, String>() {
@@ -33,17 +28,16 @@ class CaffeineExpiryTest {
                 })
                 .build(key -> new String("10"));
 
-        Assertions.assertTimeoutPreemptively(Duration.ofMinutes(1), () -> {
-            int counter = 0;
+        final long end = System.currentTimeMillis() + Duration.ofSeconds(5).toMillis();
+        int counter = 0;
 
-            String expected = cache.get(10);
-            while (true) {
-                String val = cache.get(10);
+        String expected = cache.get(10);
+        while (System.currentTimeMillis() < end) {
+            String val = cache.get(10);
 
-                assertThat("Failed after " + counter + " iterations", val, sameInstance(expected));
+            assertThat("Failed after " + counter + " iterations", val, sameInstance(expected));
 
-                counter++;
-            }
-        });
+            counter++;
+        }
     }
 }
